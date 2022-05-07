4fb3e63e9683e04f97f5c37383341cf59b894368
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-05-07 21:09:13 +0000

    chore: set up server
    Set up express server.
    Set up apollo server.
    - add express server as middleware
    - create resovlers

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  const post = new Post("First Post");
-  await orm.em.persistAndFlush(post);
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server listening at localhost:4000");
+  });
 };
 
 main().catch((err) => {
