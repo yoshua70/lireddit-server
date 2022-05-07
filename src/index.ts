ce7f32cd92c3d3f555b500be841a24d8eb0a8f69
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-05-07 23:42:08 +0000

    feat: read posts
    Query the list of posts as part of CRUD operations.

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -13,9 +14,10 @@ const main = async () => {
   const app = express();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em.fork() }),
   });
 
   await apolloServer.start();
